cb6c802d3a57c8878ba2b4dd4512fa78e8891f31
Aditya Chavan <theupsidedownportal@gmail.com>
2025-08-09 14:34:51 +0530

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, LogIn } from 'lucide-react-native';
+import { Chrome as Home, LogIn } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
